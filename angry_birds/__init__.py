a04792e83755b76626f678216e6424b192d98163
pedrofardin <pedrorf1@al.insper.edu.br>
2024-08-25 23:34:07 -0300

    teste

--- angry_birds/__init__.py
+++ angry_birds/__init__.py
@@ -1,3 +1,4 @@
+from .main import main, run_game
 from .home import HomeScreen
 from .game_over import GameOverScreen
 from .about import AboutScreen
@@ -7,4 +8,4 @@ from .force_visualizer import ForceVisualizer
 from .constants import *
 from .button import Button
 
-__all__ = ['HomeScreen', 'GameOverScreen', 'AboutScreen', 'Bird', 'Moon', 'ForceVisualizer', 'SCREEN_WIDTH', 'SCREEN_HEIGHT', 'Button']
+__all__ = ['run_game', 'main', 'HomeScreen', 'GameOverScreen', 'AboutScreen', 'Bird', 'Moon', 'ForceVisualizer', 'SCREEN_WIDTH', 'SCREEN_HEIGHT', 'Button']
